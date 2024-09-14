03fc068d3ca89f513e7019513a7f0e648e627337
monteverol <ayanbatulan2132@gmail.com>
2024-09-14 15:25:26 +0800

    added konti lang

--- frontend/src/components/input_tile.jsx
+++ frontend/src/components/input_tile.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
-const InputTile = ({ title }) => {
+const InputTile = ({ title, value, onChange, inputRef, disabled = false }) => {
     return(
         <div className="bg-[#E2ECF5] flex flex-row w-full rounded-2xl shadow-md">
             <div className="bg-[#D2E0ED] flex items-center justify-center w-60 h-24 rounded-2xl shadow-md">
                 <h1 className="text-[#1E456D] font-bold text-2xl"> {title} </h1>
             </div>
-            <input className="px-8 py-4 text-4xl font-bold w-full outline-none bg-transparent" type="text" />
+            <input 
+                className="px-8 py-4 text-4xl font-bold w-full outline-none bg-transparent" 
+                type="text" 
+                value={value} 
+                disabled={disabled} 
+                onChange={onChange} 
+                ref={inputRef} 
+            />
         </div>
     );
 }
--- frontend/src/pages/admin/book_detail.jsx
+++ frontend/src/pages/admin/book_detail.jsx
@@ -1,34 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import background from '../../assets/background_2.png';
 import AdminHeader from '../../components/admin_header';
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { GoArrowSwitch } from "react-icons/go";
+import InputTile from '../../components/input_tile';
+import Rating from '../../components/rating';
+import ReviewTile from '../../components/review_tile';
 
 const BookDetail = () => {
-    const fetchBookDetails = async (bookId) => {
-        try {
-            const response = await fetch(`https://www.googleapis.com/books/v1/volumes/${bookId}`);
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const data = await response.json();
-    
-            const { title, authors, imageLinks, averageRating, ratingsCount, description } = data.volumeInfo;
-    
-            const bookDetails = {
-                title,
-                authors: authors ? authors.join(', ') : 'Unknown Author',
-                image: imageLinks?.thumbnail || 'No Image Available',
-                averageRating: averageRating || 'No Rating',
-                ratingsCount: ratingsCount || 0,
-                description: description || 'No Description',
-            };
-    
-            console.log(bookDetails);
-            
-        } catch (error) {
-            console.error('Error fetching book details:', error);
-        }
-    };
+    const [toggleView, setToggleView] = useState(true);
+
+    const handleToggleView = () => {
+        setToggleView(toggleView ? false : true);
+    }
 
     return(
         <>
@@ -40,7 +24,7 @@ const BookDetail = () => {
                 <AdminHeader />
 
                 {/* CONTENT */}
-                <div className="bg-[#AEC6DF] h-full w-full rounded-2xl shadow-md">
+                <div className="bg-[#AEC6DF] flex-1 w-full rounded-2xl shadow-md">
                     {/* HEADER */}
                     <div className="bg-[#467DB6] w-full px-8 py-4 rounded-xl shadow-md flex flex-row justify-between items-center">
                         <div className="flex flex-row gap-8">
@@ -49,15 +33,89 @@ const BookDetail = () => {
                                 Book Details
                             </h1>
                         </div>
-                        <h1 className="text-white font-bold text-3xl hover:underline underline-offset-4">
-                            edit
-                        </h1>
+                        <div className="flex flex-row gap-8">
+                            {
+                                toggleView ?
+                                (
+                                    <h1 className="text-white font-bold text-3xl cursor-pointer hover:underline underline-offset-4">
+                                        edit
+                                    </h1>
+                                ) : null
+                            }
+                            <GoArrowSwitch color="white" size={40} className="cursor-pointer" onClick={handleToggleView}/>
+                        </div>
                     </div>
                     {/* END OF HEADER */}
                     
                     {/* CONTEXT */}
-                    <div className="flex flex-row gap-8 w-full h-full">
+                    <div className="flex flex-row gap-8 w-full h-[30rem] p-8">
+                        {/* BOOK IMAGE */}
+                        <div className="flex flex-col flex-shrink-0 h-full rounded-xl shadow-md w-[27.5rem] overflow-hidden">
+                            <div className="bg-[#E2ECF5] flex items-center justify-center h-full relative">
 
+                                {/* IMAGE */}
+                                <h1 className="text-[#6D6D6D] font-bold text-4xl text-center">BOOK COVER</h1>
+                                {/* END OF IMAGE */}
+
+                                {/* BOOK STATUS */}
+                                <div className="bg-[#BFC6CE] absolute bottom-0 left-0 w-full py-4 rounded-xl shadow-md flex items-center justify-center opacity-90">
+                                    <h1 className="text-[#1E456D] font-bold text-2xl">Borrowed</h1>
+                                </div>
+                                {/* END OF BOOK STATUS */}
+                            </div>
+                        </div>
+                        {/* END OF BOOK IMAGE */}
+
+                        {/* RIGHT SIDE */}
+                        <div className="flex flex-col gap-8 w-full h-auto overflow-y-auto">
+
+                            {
+                                toggleView ?
+                                (
+                                    <>
+                                        <InputTile title="Title" value="Chamber of Secrets" disabled={true} />
+                                        <div className="flex flex-row gap-8 w-full">
+                                            <InputTile title="Author" value="J.K. Rowling" disabled={true} />
+                                            <div className="bg-[#E7E2D2] rounded-xl px-8 py-4 flex items-center justify-center shadow-md">
+                                                <Rating value={3.1} />
+                                            </div>
+                                        </div>
+                                        {/* USER REVIEWS */}
+                                        <div className="flex flex-col gap-8">
+                                            <h1 className="text-[#4E5155] font-bold text-2xl">User Reviews:</h1>
+                                            {/* ITERATE REVIEWS */}
+                                            <ReviewTile 
+                                                name="Cayetano, Shadnilu"
+                                                rate={5}
+                                                statement="tite"
+                                            />
+                                            <ReviewTile 
+                                                name="Santiago, Shandilu"
+                                                rate={3}
+                                                statement="./."
+                                            />
+                                            <ReviewTile 
+                                                name="Sarah Dudirty"
+                                                rate={1}
+                                                statement="Shiminet Shiminet Shiminet, Shalalala~"
+                                            />
+                                            {/* END OF ITERATE REVIEWS */}
+                                        </div>
+                                        {/* END OF USER REVIEWS */}
+                                    </>
+                                ) : (
+                                    <>
+                                        <div className="bg-[#DEE2E5] w-full h-full rounded-xl shadow-md flex flex-col">
+                                            <div className="bg-white w-full px-8 py-4 rounded-xl shadow-md">
+                                                <h1 className="text-[#265787] font-bold text-2xl">Sentiment Analysis Results Based on User Review:</h1>
+                                            </div>
+                                        </div>
+                                    </>
+                                )
+                            }
+                            
+                        </div>
+                        {/* END OF RIGHT SIDE */}
                     </div>
                     {/* END OF CONTEXT */}
 
